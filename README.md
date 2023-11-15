dd99b73d2ad911827e1ebc5dc906788ba1a7ef3f
jumozaga <jumozaga@yahoo.com>
2023-11-15 18:38:02 -0300

    Project finished v1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# MatchIBM
+# IBM Match
+## Projeto 2 - Conversor de moedas
+### Python, Flask, Jinja, HTML/CSS, Consumo de Api
---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/converter', methods=['POST'])
+def converter():
+    valor = round(float(request.form.get('valor')),3)
+    moeda_origem = request.form.get('moeda_origem')
+    moeda_destino = request.form.get('moeda_destino')
+    apikey = 'fca_live_d4Jp8SDSGPK3y4FyeUcqFr2KdF3T3Ac407ZcIr2a'
+
+    # Chame a API para obter a taxa de câmbio
+    api_url = f"https://api.freecurrencyapi.com/v1/latest?apikey={apikey}&base_currency={moeda_origem}&currencies={moeda_destino}"
+    response = requests.get(api_url)
+    data = response.json()
+    taxa_cambio = round(data['data'][f'{moeda_destino}'],3)    
+    valor_convertido = round(float(valor),3) * round(float(taxa_cambio),3)
+    
+    return render_template('result.html', valor_convertido=valor_convertido, moeda_destino=moeda_destino, taxa_cambio=taxa_cambio, valor=valor, moeda_origem=moeda_origem)
+
+if __name__ == '__main__':
+    app.run(debug=True)
